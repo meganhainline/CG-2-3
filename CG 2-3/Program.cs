51111f1cdde0ca64d618bb005f1852f5ffa4244d
meganhainline <meg.hainline@gmail.com>
2018-07-22 21:14:06 -0500

    Add project files.

---
+++ CG 2-3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
